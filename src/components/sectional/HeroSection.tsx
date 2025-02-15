99b81251dc3bad4a864536d4bd17a76920f3afa3
RTIN <118107221+Romo3@users.noreply.github.com>
2025-02-15 03:47:46 -0500

    Minor

--- src/components/sectional/HeroSection.tsx
+++ src/components/sectional/HeroSection.tsx
@@ -21,7 +21,7 @@ function HeroSection() {
   return (
     <section>
       <section className="relative flex flex-col items-start justify-center text-left px-8 h-[48vh] md:h-[75vh] bg-index">
-        <div className="custom-shape-divider-bottom-1739550541">
+        <div className="custom-shape-divider-bottom-1739608965">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -35,7 +35,7 @@ function HeroSection() {
           </svg>
         </div>
         {/* Main Animated Words */}
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2 md:gap-6 -translate-y-6 md:-translate-y-20 ">
           <h1 className="text-3xl md:text-6xl font-bold animate-word1">Eat.</h1>
           <h1 className="text-3xl md:text-6xl font-bold animate-word2">
             Sleep.
@@ -49,7 +49,7 @@ function HeroSection() {
         </div>
 
         {/* Extra Animated Text (Appears after the main words) */}
-        <p className="mt-8 text-xl md:text-3xl text-white/80 opacity-0 animate-text">
+        <p className="mt-0 md:mt-8 text-md font-semibold md:text-3xl text-white/80 opacity-0 animate-text">
           Build your future, stay consistent, and level up your skills.
         </p>
       </section>
